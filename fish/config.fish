81c261dc52a6f366f29ac3c39ef3deb853ad8d07
Jhagas Han Winaya <76400782+jhagas@users.noreply.github.com>
2021-02-24 09:35:09 +0700

    My-config

---
+++ fish/config.fish
@@ -0,0 +1,15 @@
+# Aliases
+alias nano vim
+alias cmatrix 'cmatrix -a -u 2'
+alias ls 'ls -al'
+
+# Directory aliases
+#set d /home/jhagas/Mount/Data
+#set lb /home/jhagas/.local/bin
+
+#export SPICE#TIFY_INSTA#LL="/home/jhagas/spicetify-cli"
+#export PATH="$SPICETIFY_INSTALL:$PATH"
+
+# Opening Messages
+hai2 Jhagas
+echo
