cd85c0ef144886024275f01ad800a1b91a66f5b9
jhagas <jhagas@e.email>
2021-04-20 17:20:38 +0700

    Thinkfan

---
+++ Thinkfan/systemd.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+sudo pacman -Syu lm-sensors thinkfan
+
+echo "options thinkpad_acpi fan_control=1" > /etc/modprobe.d/thinkfan.conf
+modprobe thinkpad_acpi
+
+sudo systemctl enable lm_sensors
+sudo systemctl enable thinkfan
